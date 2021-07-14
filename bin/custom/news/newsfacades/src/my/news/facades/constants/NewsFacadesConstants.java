a62eea05141bd63609e5df265f9ceb88a30feab4
denis.volkov <denis.volkov@accenture.com>
2021-07-14 09:28:21 +0300

    workbench with my extensions

---
+++ bin/custom/news/newsfacades/src/my/news/facades/constants/NewsFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package my.news.facades.constants;
+
+/**
+ * Global class for all NewsFacades constants.
+ */
+public class NewsFacadesConstants extends GeneratedNewsFacadesConstants
+{
+	public static final String EXTENSIONNAME = "newsfacades";
+
+	private NewsFacadesConstants()
+	{
+		//empty
+	}
+}
---
+++ bin/custom/news/newsinitialdata/src/my/news/initialdata/constants/NewsInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package my.news.initialdata.constants;
+
+/**
+ * Global class for all NewsInitialData constants.
+ */
+public final class NewsInitialDataConstants extends GeneratedNewsInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "newsinitialdata";
+
+	private NewsInitialDataConstants()
+	{
+		//empty
+	}
+}
